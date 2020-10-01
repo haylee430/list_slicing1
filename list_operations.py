c349e5606e25fa5bbb6dc66d85fe8b7603796866
Haylee Thomas-Kuhlmann <hay.design.lee@gmail.com>
2020-10-01 12:02:45 -0500

    added all class work, lab incomplete

--- list_operations.py
+++ list_operations.py
@@ -2,106 +2,45 @@
 
 
 def head(input_list):
-    """Return the first item of the input list.
+    """Return the first item of the input list."""
 
-    For example:
-
-      >>> head(['Jan', 'Feb', 'Mar'])
-      'Jan'
-    """
-
-    return None
+    return input_list[0]
 
 
 def tail(input_list):
-    """Return a new list of all items, excluding the first item.
+    """Return a new list of all items, excluding the first item."""
 
-    For example:
-
-    >>> tail(['Jan', 'Feb', 'Mar'])
-    ['Feb', 'Mar']
-
-    """
-
-    return []
+    return input_list(range(1))
 
 
 def last(input_list):
-    """Return the last item of the input list.
+    """Return the last item of the input list."""
 
-    For example:
-
-    >>> last(['Jan', 'Feb', 'Mar'])
-    'Mar'
-
-    """
-
-    return []
-
-
-def top(input_list):
-    """Return all elements of the input list except the last.
-
-    For example:
-
-    >>> top(['Jan', 'Feb', 'Mar'])
-    ['Jan', 'Feb']
-
-    """
-
-    return []
+    return input_list(len(input_list)-1)
 
 
 def first_three(input_list):
-    """Return the first three elements of the input list.
+    """Return the first three elements of the input list."""
 
-    For example:
-
-    >>> first_three(['Jan', 'Feb', 'Mar', 'Apr', 'May'])
-    ['Jan', 'Feb', 'Mar']
-
-    """
-
-    return []
+    return input_list[0:3]
 
 
 def last_five(input_list):
-    """Return the last five elements of the input list.
+    """Return the last five elements of the input list."""
 
-    For example:
-
-    >>> last_five([0, 3, 6, 9, 12, 15, 18, 21, 24, 27])
-    [15, 18, 21, 24, 27]
-
-    """
-
-    return []
+    return input_list[(len(input_list)-1):-5]
 
 
 def middle(input_list):
-    """Return all elements of input_list except the first two and the last two.
+    """Return all elements of input_list except the first two and the last two."""
 
-    For example:
-
-    >>> middle([0, 3, 6, 9, 12, 15, 18, 21, 24, 27])
-    [6, 9, 12, 15, 18, 21]
-
-    """
-
-    return []
+    return  input_list[3:-2]
 
 
 def inner_four(input_list):
-    """Return the third, fourth, fifth, and sixth elements of input_list.
+    """Return the third, fourth, fifth, and sixth elements of input_list."""
 
-    For example:
-
-    >>> inner_four([0, 3, 6, 9, 12, 15, 18, 21, 24, 27])
-    [6, 9, 12, 15]
-
-    """
-
-    return []
+    return input_list[2:6]
 
 
 def inner_four_end(input_list):
@@ -111,41 +50,26 @@ def inner_four_end(input_list):
     described above.
 
     For example:
-
+                      
     >>> inner_four_end([0, 3, 6, 9, 12, 15, 18, 21, 24, 27])
-    [12, 15, 18, 21]
-
+    [12, 15, 18, 21]                                       
     """
 
-    return []
+    return input_list[-6:-2]
 
 
 def replace_head(input_list):
-    """Replace the head of input_list with the value 42 and return nothing.
-
-    For example:
-
-    >>> multiples = [0, 3, 6, 9, 12, 15, 18, 21, 24, 27]
-    >>> replace_head(multiples)
-    >>> multiples == [42, 3, 6, 9, 12, 15, 18, 21, 24, 27]
-    True
-
-    """
+    """Replace the head of input_list with the value 42 and return nothing."""
+    input_list[0] = 42
 
     pass
 
 
 def replace_third_and_last(input_list):
-    """Replace third and last elements of input_list with 37 and return nothing.
+    """Replace third and last elements of input_list with 37 and return nothing."""
 
-    For example:
-
-    >>> multiples = [0, 3, 6, 9, 12, 15, 18, 21, 24, 27]
-    >>> replace_third_and_last(multiples)
-    >>> multiples == [0, 3, 37, 9, 12, 15, 18, 21, 24, 37]
-    True
-
-    """
+    input_list[2]=37
+    input_list[len(input_list)-1]=37
 
     pass
 
@@ -165,6 +89,14 @@ def replace_middle(input_list):
     True
 
     """
+
+    in1 = input_list[0:3]
+    in2= input_list[len(input_list)-2]
+    int1.append([42,37])
+    int1.extend(in2)
+
+
+
 
     pass
 
@@ -209,3 +141,5 @@ if __name__ == '__main__':
     result = doctest.testmod()
     if result.failed == 0:
         print('ALL TESTS PASSED')
+
+
